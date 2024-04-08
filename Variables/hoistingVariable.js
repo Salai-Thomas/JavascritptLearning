363f511aeb9b9867f7a6eced1cc5458fafdef7d7
Salaithomas <88831835+Salai-Thomas@users.noreply.github.com>
2024-04-08 09:28:38 +0700

    Create hoistingVariable.js

---
+++ Variables/hoistingVariable.js
@@ -0,0 +1,10 @@
+//Hoisting Variable OR Lifiting Variable [PWD 206-207]
+
+console.log(num1 + num2);
+var num1 = 5
+var num2 = 6
+
+//if you declared with let => ReferenceError: Cannot access 'num1' before initialization
+//if you declared with var => NaN
+
+//var variable မှာအပေါ်တင်ပေးတဲ့ effect ရှိ
